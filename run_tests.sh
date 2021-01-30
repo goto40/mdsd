a062e451a2482ca3a66e4cc50d2545f034c5dc28
goto40 <pierre.bayerl@googlemail.com>
2021-01-30 16:04:30 +0100

    fix

--- run_tests.sh
+++ run_tests.sh
@@ -1,43 +1,55 @@
 #!/bin/bash
 
 {
-cd item_lang
+cd framework/item_lang
 ./run_tests.sh || exit 1
 cd -
 }
 
 {
-cd algo_lang
+cd framework/item_codegen_cpp
 ./run_tests.sh || exit 1
 cd -
 }
 
 {
-cd mdsd_support_library_cpp
+cd framework/item_codegen_python
 ./run_tests.sh || exit 1
 cd -
 }
 
+#{
+#cd framework/algo_lang
+#./run_tests.sh || exit 1
+#cd -
+#}
+
 {
-cd mdsd_support_library_python
+cd framework/mdsd_support_library_cpp
 ./run_tests.sh || exit 1
 cd -
 }
 
-{
-cd project_tool
-./run_tests.sh || exit 1
-cd -
-}
+#{
+#cd framework/mdsd_support_library_python
+#./run_tests.sh || exit 1
+#cd -
+#}
 
-{
-cd cpp_python_demo
-./run_tests.sh || exit 1
-cd -
-}
+#{
+#cd framework/project_tool
+#./run_tests.sh || exit 1
+#cd -
+#}
 
-{
-cd motion_model_m4
-./run_tests.sh || exit 1
-cd -
-}
+#{
+#cd framework/cpp_python_demo
+#./run_tests.sh || exit 1
+#cd -
+#}
+
+#{
+#cd framework/motion_model_m4
+#./run_tests.sh || exit 1
+#cd -
+#}
