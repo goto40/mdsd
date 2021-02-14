89fd551475b6d21d38cef8b5ec01a2a40b729a19
goto40 <pierre.bayerl@googlemail.com>
2021-02-14 16:38:23 +0100

    set default values

--- framework/item_codegen_cpp/item_codegen_cpp/common.py
+++ framework/item_codegen_cpp/item_codegen_cpp/common.py
@@ -68,6 +68,8 @@ def get_property_constexpr(a, pname):
     if t is str:
         return "\"{}\"".format(v)
     else:
+        if isinstance(v, bool):
+            v = int(v)
         return f"{get_cpp_return_type(t)}{{ {v} }}"  # TODO, better: return enum-value directly (same for python code)
 
 
