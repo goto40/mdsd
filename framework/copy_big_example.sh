b84fe28de12bf8986c1e9b6a6a315b63f13e349d
goto40 <pierre.bayerl@googlemail.com>
2021-02-19 14:54:01 +0100

    wip

---
+++ framework/copy_big_example.sh
@@ -0,0 +1,3 @@
+cp ./mdsd_support_library_common/model/big_example.item ./item_codegen_cpp/tests/model/big_example.item
+cp ./mdsd_support_library_common/model/big_example.item ./item_lang/tests/model/big_example.item
+
