f42bdcfab1d89c2cb0ee522e054864d7ff79205f
goto40 <pierre.bayerl@googlemail.com>
2021-02-19 15:49:24 +0100

    refactored test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,68 +1,26 @@
 #!/bin/bash
 
-{
-cd framework/item_lang
-./run_tests.sh || exit 1
-cd -
-}
+TestDirs=(
 
-{
-cd framework/item_codegen_cpp
-./run_tests.sh || exit 1
-cd -
-}
-
-{
-cd framework/item_codegen_python
-./run_tests.sh || exit 1
-cd -
-}
-
-{
-cd framework/algo_lang
-./run_tests.sh || exit 1
-cd -
-}
-
-{
-cd framework/algo_codegen_cpp
-./run_tests.sh || exit 1
-cd -
-}
-
-{
-cd framework/algo_codegen_python
-./run_tests.sh || exit 1
-cd -
-}
-
-{
-cd framework/mdsd_support_library_cpp
-./run_tests.sh || exit 1
-cd -
-}
-
-{
-cd framework/mdsd_support_library_python
-./run_tests.sh || exit 1
-cd -
-}
-
-{
-cd framework/project_tool
-./run_tests.sh || exit 1
-cd -
-}
-
-{
-cd examples/cpp_python_demo
-./run_tests.sh || exit 1
-cd -
-}
-
-#{
-#cd framework/motion_model_m4
-#./run_tests.sh || exit 1
-#cd -
-#}
+	"framework/item_lang"
+	"framework/item_codegen_cpp"
+	"framework/item_codegen_python"
+	
+	"framework/algo_lang"
+	"framework/algo_codegen_cpp"
+	"framework/algo_codegen_python"
+	
+	"framework/mdsd_support_library_python"
+	"framework/mdsd_support_library_cpp"
+	
+	"framework/project_tool"
+)
+ 
+for d in ${TestDirs[@]}; do
+	{
+	cd $d
+	./run_tests.sh || exit 1
+	cd -
+	}
+done
 
