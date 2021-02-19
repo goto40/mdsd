7bd02c1c9522f3990618ab7c0ed7268740016b4f
goto40 <pierre.bayerl@googlemail.com>
2021-02-19 17:19:55 +0100

    test workflow

---
+++ install_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pip install -e framework/codegen_test_support || exit 1
+pip install -e framework/item_lang || exit 1
+pip install -e framework/item_codegen_cpp || exit 1
+pip install -e framework/item_codegen_python || exit 1
+pip install -e framework/algo_lang || exit 1
+pip install -e framework/mdsd_support_library_python || exit 1
+pip install -e framework/project_tool || exit 1
+
