d6a0ae349a6e271e403891db2552bcd93bf5474d
goto40 <pierre.bayerl@googlemail.com>
2021-02-20 17:22:29 +0100

    fix

--- framework/mdsd_support_library_python/run_tests.sh
+++ framework/mdsd_support_library_python/run_tests.sh
@@ -4,8 +4,8 @@ rm -rf src-gen
 textx generate ../mdsd_support_library_common/model/*.item --overwrite --target python  --output-path src-gen || exit 1
 export PYTHONPATH=$(pwd)/src-gen
 ls $PYTHONPATH
-coverage run --omit tests --omit venv --source mdsd_support_library -m py.test --junit-xml=test_results.xml tests || exit 1
-coverage report --fail-under 90 # || exit 1
+coverage run --omit tests --omit venv --source mdsd -m py.test --junit-xml=test_results.xml tests || exit 1
+coverage report --fail-under 70 # || exit 1
 coverage xml || exit 1
 
 # Run this to generate html report
