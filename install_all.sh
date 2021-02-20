eea48c7b8c35e50359862d3c78f135df51f12634
goto40 <pierre.bayerl@googlemail.com>
2021-02-20 12:42:12 +0100

    test workflow

--- install_all.sh
+++ install_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install -e framework/codegen_test_support || exit 1
-pip install -e framework/item_lang || exit 1
+pip install -e framework/item_lang[dev] || exit 1
 pip install -e framework/item_codegen_cpp || exit 1
 pip install -e framework/item_codegen_python || exit 1
 pip install -e framework/algo_lang || exit 1
