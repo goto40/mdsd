9d45d75141992cc8e8074b05a2e3382020efae28
goto40 <pierre.bayerl@googlemail.com>
2021-02-20 17:34:58 +0100

    excluded cpp results

--- examples/image_tool/run_tests.sh
+++ examples/image_tool/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sh build.sh || exit 1
-./build/my_image_lib/Tester.exe -r console -r xml -o test_results.xml || exit 1
+./build/my_image_lib/Tester.exe -r console -r xml -o test_results2.xml || exit 1
 
--- framework/mdsd_support_library_cpp/run_tests.sh
+++ framework/mdsd_support_library_cpp/run_tests.sh
@@ -7,9 +7,9 @@ cd build
 
 if ! command -v mycmake &> /dev/null
 then
-	cmake -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=ON ..&& make && ./Tester.exe -r console -r xml -o test_results.xml || exit 1
+	cmake -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=ON ..&& make && ./Tester.exe -r console -r xml -o test_results2.xml || exit 1
 else
-	mycmake -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=ON ..&& make && ./Tester.exe -r console -r xml -o test_results.xml || exit 1
+	mycmake -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=ON ..&& make && ./Tester.exe -r console -r xml -o test_results2.xml || exit 1
 fi
 
 cd -
