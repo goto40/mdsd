49b583f24b879d876fd077c97bf20878b60d3193
goto40 <pierre.bayerl@googlemail.com>
2021-02-20 15:23:29 +0100

    flake8

--- framework/algo_codegen_python/tests/test_generated_code_mean_algo.py
+++ framework/algo_codegen_python/tests/test_generated_code_mean_algo.py
@@ -2,10 +2,11 @@ from P1.MeanAlgo import MeanAlgo
 from Items.MeanAlgoInput import MeanAlgoInput as I
 from Items.MeanAlgoOutput import MeanAlgoOutput as O
 
+
 def test_custom_impl_of_MeanAlgo():
     class MyMeanAlgo(MeanAlgo):
         def compute(self, inp1: I, inp2: I, oup: O):
-            outp.value = (inp1.value + inp2.value)/2
+            outp.value = (inp1.value + inp2.value) / 2
 
     a = MyMeanAlgo()
     inp1 = I(9)
