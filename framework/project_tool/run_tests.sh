f2396d114bf954f56c68b4541673c218dc9d9beb
goto40 <pierre.bayerl@googlemail.com>
2021-02-20 15:34:14 +0100

    script fix

--- framework/project_tool/run_tests.sh
+++ framework/project_tool/run_tests.sh
@@ -1,4 +1,4 @@
 pip install -e .
-coverage run --source swig_tool -m py.test tests || exit 1
+coverage run --source project_tool -m py.test tests || exit 1
 coverage report --fail-under 90 # || exit 1
 echo "OK"
