c47feffbea1ee5efd3505c7f5738cb81a8b86c80
goto40 <pierre.bayerl@googlemail.com>
2021-02-21 17:33:08 +0100

    initial impl

--- examples/image_tool/my_image_lib/src/my_image_lib/background_subtraction/AlgoBackgroundSubtractionImpl.h
+++ examples/image_tool/my_image_lib/src/my_image_lib/background_subtraction/AlgoBackgroundSubtractionImpl.h
@@ -17,7 +17,22 @@ public:
         const my_image_lib::GrayImage& input, 
         my_image_lib::background_subtraction::BackgroundSubtractionResults& output
     ) {
-
+        my_image_lib::Tictoc tictoc{"median2D"};
+        my_image_lib::median2D( input, params.x, params.y, output.threshold, 4 );
+        for (auto &pixel: output.threshold.pixel) {
+            pixel = pixel + params.threshold;
+        }
+        output.result.w = output.threshold.w;
+        output.result.h = output.threshold.h;
+        output._GET_WRAPPER().adjust_array_sizes_and_variants();
+        for (size_t idx=0;idx<output.result.pixel.size(); idx++) {
+            if (input.pixel[idx]<output.threshold.pixel[idx]) {
+                output.result.pixel[idx]=0.0;
+            }
+            else {
+                output.result.pixel[idx]=1.0;
+            }
+        }
     }
 };
 
