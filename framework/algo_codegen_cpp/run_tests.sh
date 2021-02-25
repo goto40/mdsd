aaa786f266df1ad7534dcc22ebf17a135749f98d
goto40 <pierre.bayerl@googlemail.com>
2021-02-25 18:52:20 +0100

    fixed coverage check

--- framework/algo_codegen_cpp/run_tests.sh
+++ framework/algo_codegen_cpp/run_tests.sh
@@ -1,7 +1,7 @@
 pip install -e ../item_lang
 pip install -e .
-coverage run --source item_lang -m py.test --junit-xml=test_results.xml tests || exit 1
+coverage run --source algo_codegen_cpp -m py.test --junit-xml=test_results.xml tests || exit 1
 flake8 || exit 1
-coverage report --fail-under 90 # || exit 1
+coverage report --fail-under 80 || exit 1
 coverage xml || exit 1
 echo "OK"
--- framework/algo_lang/run_tests.sh
+++ framework/algo_lang/run_tests.sh
@@ -1,6 +1,6 @@
 pip install -e ../item_lang
 pip install -e .
-coverage run --source item_lang -m py.test --junit-xml=test_results.xml tests || exit 1
-coverage report --fail-under 90 # || exit 1
+coverage run --source algo_lang -m py.test --junit-xml=test_results.xml tests || exit 1
+coverage report --fail-under 80 || exit 1
 coverage xml || exit 1
 echo "OK"
