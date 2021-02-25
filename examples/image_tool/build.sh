ffdc5a96f831822d6bbbb9a4f240b445c50ad070
goto40 <pierre.bayerl@googlemail.com>
2021-02-25 19:08:14 +0100

    wip

--- examples/image_tool/build.sh
+++ examples/image_tool/build.sh
@@ -13,8 +13,10 @@ cd ..
 
 rm -rf build/swig_proj
 mkdir -p build/swig_proj
-projecttool python-i-file --require-pattern="ACTIVATE FOR SWIG" my_image_lib_swig build/swig_proj my_image_lib/src my_image_lib/src-gen ../../framework/mdsd_support_library_cpp/src/
-swig -c++ -python -I../../framework/mdsd_support_library_cpp/src -Imy_image_lib/src -Imy_image_lib/src-gen -o build/swig_proj/wrapper.cpp build/swig_proj/my_image_lib_swig.i || exit 1
+cd build/swig_proj
+projecttool python-i-file --require-pattern="ACTIVATE FOR SWIG" my_image_lib_swig . ../../my_image_lib/src ../../my_image_lib/src-gen ../../../../framework/mdsd_support_library_cpp/src/
+swig -c++ -python -I../../../../framework/mdsd_support_library_cpp/src -I../../my_image_lib/src -I../../my_image_lib/src-gen -o ./wrapper.cpp my_image_lib_swig.i || exit 1
+cd ../..
 pip install -e build/swig_proj || exit 1
 
 
