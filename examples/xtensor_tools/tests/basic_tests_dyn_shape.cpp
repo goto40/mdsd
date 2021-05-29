775b6a191cb8f1141b2395d2b2b072f9a03411ac
goto40 <pierre.bayerl@googlemail.com>
2021-05-29 10:52:28 +0200

    wip

---
+++ examples/xtensor_tools/tests/basic_tests_dyn_shape.cpp
@@ -0,0 +1,26 @@
+#include "catch.hpp"
+
+#include <xtensor/xarray.hpp>
+#include <xtensor/xio.hpp>
+
+TEST_CASE("first steps: dynamic shape", "[xtensor]")
+{
+  xt::xarray<double>::shape_type shape = {2, 3};
+  xt::xarray<double> a0(shape);
+  xt::xarray<double> a1(shape, 2.5);
+  xt::xarray<double> a2 = {{1., 2., 3.}, {4., 5., 6.}};
+  auto a3 = xt::xarray<double>::from_shape(shape);
+
+  std::cout << a0 << "\n";
+  CHECK(a0.shape()[0] == 2);
+  CHECK(a0.shape()[1] == 3);
+
+  std::cout << a1 << "\n";
+  CHECK(a1(0, 0) == Approx(2.5));
+  std::cout << a2 << "\n";
+  CHECK(a2(0, 0) == Approx(1.0));
+  CHECK(a2(0, 1) == Approx(2.0));
+  CHECK(a2(1, 2) == Approx(6.0));
+  std::cout << a3 << "\n";
+  CHECK(a3(1, 2) == Approx(0.0));
+}
