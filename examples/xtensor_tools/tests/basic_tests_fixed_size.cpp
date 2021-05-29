569caee9ea961891c6e5c8c1f1708027f26af900
goto40 <pierre.bayerl@googlemail.com>
2021-05-29 10:56:41 +0200

    wip

---
+++ examples/xtensor_tools/tests/basic_tests_fixed_size.cpp
@@ -0,0 +1,14 @@
+#include "catch.hpp"
+
+#include <xtensor/xfixed.hpp>
+#include <xtensor/xio.hpp>
+
+TEST_CASE("first steps: fixed size", "[xtensor]")
+{
+    xt::xtensor_fixed<double, xt::xshape<2, 3>> a2 = {{10., 2., 3.}, {4., 5., 6.}};
+
+    std::cout << a2 << "\n";
+    CHECK(a2(0, 0) == Approx(10.0));
+    CHECK(a2(0, 1) == Approx(2.0));
+    CHECK(a2(1, 2) == Approx(6.0));
+}
