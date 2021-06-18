4bcea21acf4b521e39cbb0ed51cca008b8b44dae
goto40 <pierre.bayerl@googlemail.com>
2021-06-18 18:06:39 +0200

    prepared code from rust to be migrated

--- examples/xtensor_tools/src/xtensor_tools/simple_integration.h
+++ examples/xtensor_tools/src/xtensor_tools/simple_integration.h
@@ -46,5 +46,67 @@ void softmax_norm_vxvy(T& motion, size_t blurn, float pow_value=1.0f, float eps=
     }
 }
 
+/*
+pub fn blur_vxvy(motion: &mut MotionBlock, blurn: usize) {
+    let shape = motion.motion_hw_vyvx.raw_dim();
+    let mask = hanningmask1d(blurn);
+    for y in 0..shape[0] {
+        for x in 0..shape[1] {
+            let blurred = create_corr2_1d(&motion.motion_hw_vyvx.slice(s![y, x, .., ..]), &mask);
+            motion
+                .motion_hw_vyvx
+                .slice_mut(s![y, x, .., ..])
+                .assign(&blurred);
+        }
+    }
+}
+
+pub fn modulate_motion(v1: &mut MotionBlock, mt: &mut MotionBlock, c: f32, f: f32, pow: f32) {
+    let shape = v1.motion_hw_vyvx.raw_dim();
+    if shape != mt.motion_hw_vyvx.raw_dim() {
+        mt.motion_hw_vyvx = Array4::zeros(shape);
+    }
+    Zip::from(&mut v1.motion_hw_vyvx)
+        .and(&mt.motion_hw_vyvx)
+        .for_each(|v1v, mtv| {
+            *v1v *= c + f * mtv.powf(pow);
+        });
+}
+
+pub fn predict_motion(motion: &mut MotionBlock) {
+    let shape = motion.motion_hw_vyvx.raw_dim();
+    for ivy in 0..shape[2] {
+        let sivy = ivy as i32;
+        let shift_y = sivy - shape[2] as i32 / 2;
+        let h = shape[0] - shift_y.abs() as usize;
+        let (ys0, yd0) = if shift_y < 0 {
+            (-shift_y as usize, 0)
+        } else {
+            (0, shift_y as usize)
+        };
+
+        for ivx in 0..shape[3] {
+            let sivx = ivx as i32;
+            let shift_x = sivx - shape[3] as i32 / 2;
+            let w = shape[1] - shift_x.abs() as usize;
+            let (xs0, xd0) = if shift_x < 0 {
+                (-shift_x as usize, 0)
+            } else {
+                (0, shift_x as usize)
+            };
+            let tmp = motion
+                .motion_hw_vyvx
+                .slice(s![ys0..ys0 + h, xs0..xs0 + w, ivy, ivx])
+                .to_owned(); // copy
+            motion
+                .motion_hw_vyvx
+                .slice_mut(s![yd0..yd0 + h, xd0..xd0 + w, ivy, ivx])
+                .assign(&tmp);
+        }
+    }
+}
+
+*/
+
 } // end namespace
 #endif
